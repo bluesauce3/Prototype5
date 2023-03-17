7974a786bbf77e78198eb227357a5f42c362fbe4
Theo <106856604+bluesauce3@users.noreply.github.com>
2023-03-18 11:36:09 +1300

    made targets fly up and able to be destroyed, added scoring

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    private int score;
+    private float spawnRate = 1.0f;
+
+    public bool isGameActive;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public List<GameObject> targets;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        isGameActive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget() {
+        while (isGameActive) {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd) {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver() {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+}
