13368fccc106f09d8ebce1eba792bebaad4aed59
Theo <106856604+bluesauce3@users.noreply.github.com>
2023-03-18 13:32:28 +1300

    made title screen and difficulties

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,13 +14,11 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
     public List<GameObject> targets;
+    public Button restartButton;
+    public GameObject titleScreen;
     // Start is called before the first frame update
     void Start()
-    {
-        StartCoroutine(SpawnTarget());
-        UpdateScore(0);
-        isGameActive = true;
-    }
+    {}
 
     // Update is called once per frame
     void Update()
@@ -42,5 +42,19 @@ public class GameManager : MonoBehaviour
     public void GameOver() {
         gameOverText.gameObject.SetActive(true);
         isGameActive = false;
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty) {
+        isGameActive = true;
+        score = 0;
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
+        spawnRate /= difficulty;
     }
 }
